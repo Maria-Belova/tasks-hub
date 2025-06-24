ff26d8dc5cb76733d2a38bd82d30ff0d78196ad0
ma.belova1 <ma.belova1@tensor.ru>
2025-06-24 23:54:54 +0300

    Add small fixes

--- src/components/ui/search-field/SearchField.tsx
+++ src/components/ui/search-field/SearchField.tsx
@@ -11,7 +11,7 @@ interface ISearchField {
 
 export const SearchField: FC<ISearchField> = ({ placeholder = 'Search something...', value, onChange }: ISearchField) => {
   return (
-    <div className='flex items-center w-full max-w-xs px-4 py-2 rounded-full bg-white dark:bg-neutral-700 shadow-sm'>
+    <div className='flex items-center w-xs px-4 py-2 rounded-full bg-white dark:bg-neutral-700 shadow-sm'>
       <Search size={20} className='text-neutual-500 mr-2' />
       <input
         className='w-full bg-transparent focus:outline-none text-sm placeholder:text-neutral-400 text-neutral-800 dark:text-white'
